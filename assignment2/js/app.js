744ebebf90f8fb9df9a24d439872af99347be384
Chenna Reddy <chenna@alavala.in>
2016-11-24 22:28:47 +0100

    working solution

--- assignment2/js/app.js
+++ assignment2/js/app.js
@@ -8,30 +8,43 @@
 
   ToBuyController.$inject = ['ShoppingListCheckOffService']
   function ToBuyController(ShoppingListCheckOffService) {
-
+    var toBuy = this;
+    toBuy.items = function() {
+      return ShoppingListCheckOffService.itemsToBuy;
+    };
+    toBuy.buyItem = function(itemIndex) {
+      ShoppingListCheckOffService.buyItem(itemIndex);
+    };
   };
 
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService']
   function AlreadyBoughtController(ShoppingListCheckOffService) {
-
+    var bought = this;
+    bought.items = function() {
+      return ShoppingListCheckOffService.itemsBought;
+    };
   };
 
   function ShoppingListCheckOffService() {
     var service = this;
 
-    var itemsToBuy = [
-      {}
+    service.itemsToBuy = [
+      {name: "cookies", quantity: 10},
+      {name: "donuts", quantity: 20},
+      {name: "chocolates", quantity: 50},
+      {name: "chips", quantity: 10},
+      {name: "ice creams", quantity: 5}
     ];
 
-    var itemsBought = [
-
+    service.itemsBought = [
     ];
 
-    service.buyItem = function (item) {
+    service.buyItem = function (itemIndex) {
+      var item = service.itemsToBuy.splice(itemIndex, 1);
+      service.itemsBought.push(item[0]);
+    };
 
-    }
-
-  }
+  };
 
 
 })();
